608b4324202b6a09335c88073d3f07399e269a96
tjh <tjh@192.168.1.11>
2026-01-14 00:31:18 +0800

    Stage 1: Initialization

--- pkg/bloom/bloom.go
+++ pkg/bloom/bloom.go
@@ -1,1 +1,15 @@
 package bloom
+
+type BloomFilter struct {
+	k      int
+	bitset []byte
+}
+
+func NewBloomFilter(k, m int) *BloomFilter {
+	numBytes := (m + 7) / 8
+
+	return &BloomFilter{
+		k:      k,
+		bitset: make([]byte, numBytes),
+	}
+}
